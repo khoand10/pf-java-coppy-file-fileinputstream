301e7e867f9ce430dcd5da799e76eb9e766b4b2f
khoand10@gmail.com <khoand10@gmail.com>
2018-04-13 19:51:31 +0700

    đọc ghi file nhị phân

---
+++ src/CoppyFile.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class CoppyFile {
+    public static void main(String[] args) throws Exception {
+        InputStream input = null;
+        OutputStream output = null;
+        File source = new File("source.txt");
+        File dest = new File("disk.txt");
+        try {
+            input = new FileInputStream(source);
+            output = new FileOutputStream(dest);
+            byte[] buf = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = input.read(buf)) > 0) {
+                output.write(buf, 0, bytesRead);
+                System.out.println(bytesRead);
+            }
+        } finally {
+            input.close();
+            output.close();
+        }
+    }
+}
